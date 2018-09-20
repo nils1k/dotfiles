46a76d264824ee3c5d4f3d95282c668d7000d6e2
Kristian Nilsen <kristian@Kristians-MBP.local>
2018-09-20 21:02:03 +0200

    Initial draft

---
+++ aliases.zsh
@@ -0,0 +1,20 @@
+# Shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias reloadcli="source $HOME/.zshrc"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
+weather() { curl -4 wttr.in/${1:-antwerp} }
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias cls="clear"
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias library="cd $HOME/Library"
+alias dev="cd $HOME/Development"
+
+# Docker
+alias dstop="docker stop $(docker ps -a -q)"
+alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
+alias dpurgeimages="docker rmi $(docker images -q)"
+dbuild() { docker build -t=$1 .; }
+dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
